161eef50759bf737461abdfcffb758b590cee753
IlhamDM <56746667+IlhamDM@users.noreply.github.com>
2020-03-28 20:46:02 +0700

    Add files via upload

---
+++ SimpleChatting/src/simplechatting/SimpleChatting.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplechatting;
+
+/**
+ * @author Ilham DM
+ */
+public class SimpleChatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Server s = new Server();
+        s.setVisible(true);
+        // TODO code application logic here
+    }
+}
